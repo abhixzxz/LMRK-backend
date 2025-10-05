d7944c81dcebe43a59d87374f1bd51853ac5f741
abhixzxz <abhi88wiz@gmail.com>
2025-10-05 22:22:06 +0530

    feat: added new backend URL

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const sql = require("mssql");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 // MS SQL config
